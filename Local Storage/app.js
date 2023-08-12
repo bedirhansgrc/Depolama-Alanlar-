49c4fac3e09807404e82db90a588fb2ff0c590e1
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-12 17:30:20 +0300

    Create app.js

---
+++ Local Storage/app.js
@@ -0,0 +1,27 @@
+//Local Storage
+
+//Set Item
+
+// localStorage.setItem("Hareket1", "Burpee")
+
+//Get Item
+
+// const value = localStorage.getItem("Hareket1")
+// console.log(value)
+
+//Clear Local Storage
+
+// localStorage.clear()
+// localStorage.getItem("Spor Hareketi")
+// if(localStorage.getItem("Spor Hareketi") === null){
+//     console.log("Aradığınız Anahtar Kelime Bulunamamıştır.")
+// }
+
+const todos = ["Todo1","Todo2","Todo3"]
+localStorage.setItem("todo", todos)
+//Arraye donüştürme
+localStorage.setItem("todo", JSON.stringify(todos))
+
+const value = JSON.parse(localStorage.getItem("todo"))
+
+console.log(value)
